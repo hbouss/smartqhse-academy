7bb6ca37d8bd793de07b97dae664fbb5c5119d97
hboussouar <hichem.boussouar@gmail.com>
2026-05-09 23:32:29 +0200

    Add LinkedIn Insight Tag

--- lms-frontend/app/layout.tsx
+++ lms-frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
@@ -86,7 +87,43 @@ export default function RootLayout({
             <SiteFooter />
           </div>
         </AuthProvider>
+
         <Analytics />
+
+        <Script id="linkedin-insight-init" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "9120474";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+
+        <Script id="linkedin-insight-loader" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l) {
+                window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q = [];
+              }
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";
+              b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=9120474&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
